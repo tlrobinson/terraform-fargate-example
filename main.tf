cbe25b783b71b78b6fb59361ddf9fe7ece2d3d82
Tom Robinson <tlrobinson@gmail.com>
2020-05-11 09:55:38 -0700

    ecs poc

--- main.tf
+++ main.tf
@@ -86,74 +86,74 @@ resource "aws_route_table_association" "private" {
 
 # ALB Security group
 # This is the group you need to edit if you want to restrict access to your application
-resource "aws_security_group" "lb" {
-  name        = "tf-ecs-alb"
-  description = "controls access to the ALB"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "lb" {
+#   name        = "tf-ecs-alb"
+#   description = "controls access to the ALB"
+#   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     protocol    = "tcp"
+#     from_port   = 80
+#     to_port     = 80
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 # Traffic to the ECS Cluster should only come from the ALB
-resource "aws_security_group" "ecs_tasks" {
-  name        = "tf-ecs-tasks"
-  description = "allow inbound access from the ALB only"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "ecs_tasks" {
+#   name        = "tf-ecs-tasks"
+#   description = "allow inbound access from the ALB only"
+#   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    protocol        = "tcp"
-    from_port       = var.app_port
-    to_port         = var.app_port
-    security_groups = [aws_security_group.lb.id]
-  }
+#   ingress {
+#     protocol        = "tcp"
+#     from_port       = var.app_port
+#     to_port         = var.app_port
+#     security_groups = [aws_security_group.lb.id]
+#   }
 
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     protocol    = "-1"
+#     from_port   = 0
+#     to_port     = 0
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 ### ALB
 
-resource "aws_alb" "main" {
-  name            = "tf-ecs-chat"
-  subnets         = aws_subnet.public.*.id
-  security_groups = [aws_security_group.lb.id]
-}
+# resource "aws_alb" "main" {
+#   name            = "tf-ecs-chat"
+#   subnets         = aws_subnet.public.*.id
+#   security_groups = [aws_security_group.lb.id]
+# }
 
-resource "aws_alb_target_group" "app" {
-  name        = "tf-ecs-chat"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "ip"
-}
+# resource "aws_alb_target_group" "app" {
+#   name        = "tf-ecs-chat"
+#   port        = 80
+#   protocol    = "HTTP"
+#   vpc_id      = aws_vpc.main.id
+#   target_type = "ip"
+# }
 
-# Redirect all traffic from the ALB to the target group
-resource "aws_alb_listener" "front_end" {
-  load_balancer_arn = aws_alb.main.id
-  port              = "80"
-  protocol          = "HTTP"
+# # Redirect all traffic from the ALB to the target group
+# resource "aws_alb_listener" "front_end" {
+#   load_balancer_arn = aws_alb.main.id
+#   port              = "80"
+#   protocol          = "HTTP"
 
-  default_action {
-    target_group_arn = aws_alb_target_group.app.id
-    type             = "forward"
-  }
-}
+#   default_action {
+#     target_group_arn = aws_alb_target_group.app.id
+#     type             = "forward"
+#   }
+# }
 
 ### ECS
 
@@ -187,25 +187,25 @@ resource "aws_ecs_task_definition" "app" {
 DEFINITION
 }
 
-resource "aws_ecs_service" "main" {
-  name            = "tf-ecs-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = var.app_count
-  launch_type     = "FARGATE"
+# resource "aws_ecs_service" "main" {
+#   name            = "tf-ecs-service"
+#   cluster         = aws_ecs_cluster.main.id
+#   task_definition = aws_ecs_task_definition.app.arn
+#   desired_count   = var.app_count
+#   launch_type     = "FARGATE"
 
-  network_configuration {
-    security_groups = [aws_security_group.ecs_tasks.id]
-    subnets         = aws_subnet.private.*.id
-  }
+#   network_configuration {
+#     security_groups = [aws_security_group.ecs_tasks.id]
+#     subnets         = aws_subnet.private.*.id
+#   }
 
-  load_balancer {
-    target_group_arn = aws_alb_target_group.app.id
-    container_name   = "app"
-    container_port   = var.app_port
-  }
+#   load_balancer {
+#     target_group_arn = aws_alb_target_group.app.id
+#     container_name   = "app"
+#     container_port   = var.app_port
+#   }
 
-  depends_on = [
-    aws_alb_listener.front_end
-  ]
-}
+#   depends_on = [
+#     aws_alb_listener.front_end
+#   ]
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,18 @@
-output "alb_hostname" {
-  value = aws_alb.main.dns_name
-}
+# output "alb_hostname" {
+#   value = aws_alb.main.dns_name
+# }
 
-output "network_configuration" {
-  value = aws_ecs_service.main.network_configuration
-}
+# output "network_configuration" {
+#   value = aws_ecs_service.main.network_configuration
+# }
 
 output "cluster" {
   value = aws_ecs_cluster.main.name
 }
 
-output "service" {
-  value = aws_ecs_service.main.name
-}
+# output "service" {
+#   value = aws_ecs_service.main.name
+# }
 
 # output "task" {
 #   value = aws_ecs_task_definition.app
